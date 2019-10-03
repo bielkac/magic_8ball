e124d5dc881658a6617596d695d016508f9c5f30
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-10-03 09:51:40 -0400

    started else if with numbers combined

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -11,9 +11,16 @@ public class Main {
         keyboard = new Scanner(System.in);
         System.out.println(" Choose a number between 1-100");
         firstNumber = keyboard.nextInt();
-       
-
+        if (10 >= firstNumber && firstNumber > 0) {
+            System.out.println("uncertain");
         }
+        else if(20>=firstNumber && firstNumber>10) {
+            System.out.println("not likely");
+        }
+        else if (30>= firstNumber && firstNumber>20){
+            System.out.println(" negative outcome ");
+        }
+        
     }
 
 }
