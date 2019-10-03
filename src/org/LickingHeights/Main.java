2d01a4b99c2f78d96c2af149c2bed9866920655f
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-10-03 09:47:08 -0400

    made it so user could put in a number

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,19 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // declare
+        Scanner keyboard;
+        int firstNumber;
+        keyboard = new Scanner(System.in);
+        System.out.println(" Choose a number between 1-100");
+        firstNumber = keyboard.nextInt();
+       
+
+        }
+    }
+
+}
